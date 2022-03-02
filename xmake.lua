420551447ed433e6702af7adeb1001c874fc2797
Mai-Lapyst <info@lapyst.by>
2022-03-02 13:52:43 +0100

    Fixing xmake.lua so we dont accidently use the system libtabfs; also using clang for specs for now

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 add_requires("cxxspec")
 
+-- public target that gets installed & should be used by any tool that also lives outside of this repo
 target("libtabfs")
     set_kind("static")
     add_files("src/*.c")
@@ -14,13 +15,24 @@ target("libtabfs")
         add_defines("DEBUG")
     end
 
+-- local target that is used for the specs to enable more debugging capabilities and
+-- also so that xmake dosnt use the system-wide installed libtabfs!
+target("local_libtabfs")
+    set_default(false)
+    set_kind("static")
+    add_files("src/*.c")
+    add_includedirs("include", {public = true})
+    add_defines("DEBUG")
+    add_defines("LIBTABFS_DEBUG_PRINTF")
+
 target("specs")
     set_default(false)
     set_kind("binary")
-    add_deps("libtabfs")
+    add_deps("local_libtabfs")
     add_packages("cxxspec")
     add_files("specs/*.cpp", "utils/dump.cpp")
     add_includedirs("utils")
+    set_toolchains("clang")
 
 target("tabfs_inspect")
     set_kind("binary")
